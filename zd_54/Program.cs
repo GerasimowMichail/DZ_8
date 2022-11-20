ae7bfb2ec44d6b5b77dd58dccc758a74c53c6b36
Mischa <GerasimowMichail@yandex.ru>
2022-11-20 17:17:13 +0300

    Добавил файл gitignore

---
+++ zd_54/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Write("Введите число строк массива: ");
+int row=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов массива: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Формируем массив заполненный случайным образом :");
+
+int [,] FillArray(int row, int column)
+{
+   
+    int [,] massiv= new int [row,column];
+
+    for(int i=0; i<row;i++)
+    {
+        for(int j=0; j<column; j++)
+        {
+            massiv[i,j]=new Random().Next(0,10);
+        }
+    }
+    return massiv;
+}
+
+void PrintArray(int [,] massiv)
+{
+    for(int i=0; i<row; i++)
+    {
+        for(int j=0; j<column; j++)
+        {
+            Console.Write($"{massiv[i,j]}"+ " ");
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] array = FillArray(row, column);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Отсортированый массив");
+
+int [,] arraySort=SortMassiv(array);
+PrintArray(arraySort);
+
+int [,] SortMassiv(int [,] massiv)
+{   for (int k=0; k<((row-1)*(column)); k++)
+        {
+            for(int i=0; i<row; i++)
+            { int j=0;
+                for ( j=0; j<column-1; j++)
+                {
+                    if (massiv[i,j]>massiv[i,j+1])
+                    {
+                        int temp = massiv[i, j+1];
+                        massiv[i,j+1]=massiv[i,j];
+                        massiv[i,j]=temp;
+                        
+                    }                    
+                }
+            
+            }
+        }
+
+    return massiv;
+}
---
+++ zd_56/Program.cs
@@ -0,0 +1,104 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Console.Write("Введите число строк массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов массива: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Формируем массив заполненный случайным образом :");
+
+int[,] FillArray(int row, int column)
+{
+
+    int[,] massiv = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            massiv[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return massiv;
+}
+
+void PrintArray(int[,] massiv)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{massiv[i, j]}" + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void PrintArray1(int[] massiv)
+{
+
+    for (int j = 0; j < column; j++)
+    {
+        Console.Write($"{massiv[j]}" + " ");
+
+    }
+    Console.WriteLine();
+
+}
+
+int[,] array = FillArray(row, column);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Сумма элементов строки");
+int[] masLin1 = MasSum(array);
+// PrintArray1(masLin1);
+Console.WriteLine();
+
+//Поиск суммы строк. возвращает массив сумм строк
+
+int[] MasSum(int[,] massiv)
+{
+    int[] masLin = new int[row + 1];
+    for (int i = 0; i < row; i++)
+    {
+        int j = 0;
+        int sum = 0;
+        for (j = 0; j < column; j++)
+        {
+            sum = sum + massiv[i, j];
+        }
+        Console.WriteLine(sum);
+        masLin[i] = sum;
+    }
+    return masLin;
+}
+
+int [] FindMinStr(int []array1)
+{
+int minSumStr = array1[0];
+int index=0;
+int [] printMasiv=new int[2];
+for (int k = 0; k < row - 1; k++)
+{
+    if (minSumStr > array1[k + 1])
+    {
+        minSumStr = array1[k + 1];
+       printMasiv[1]=k+1;
+        printMasiv[0]=minSumStr;
+        // Console.WriteLine(k);
+        // Console.WriteLine(array1[k + 1]);
+    }
+}
+return printMasiv;
+
+}
+  Console.WriteLine();
+
+Console.WriteLine($"Минимальная сумма в строке {FindMinStr(masLin1)}");
